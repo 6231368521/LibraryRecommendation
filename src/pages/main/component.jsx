7a41bd5e8c70f64115df78c8b38fd5209d01c0fa
Boss <boss_12389@hotmail.com>
2023-01-14 20:47:17 +0700

    add fack data

--- src/pages/main/component.jsx
+++ src/pages/main/component.jsx
@@ -1,15 +1,15 @@
 
 import Card from 'react-bootstrap/Card';
+import { faker } from '@faker-js/faker';
 
 export const CardCustom = ()=>{
   return(
-    <Card style={{ width: '18rem',margin:'10px' }}>
+    <Card style={{ width: '18rem',margin:'10px',height:'200px' }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>{faker.name.fullName()}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Catagory</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {faker.music.genre()}
         </Card.Text>
         <Card.Link href="#">Card Link</Card.Link>
         <Card.Link href="#">Another Link</Card.Link>
