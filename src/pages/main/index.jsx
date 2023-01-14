c7d6b629e393fa2ce40f707f55411b4f9f3af3f5
Boss <boss_12389@hotmail.com>
2023-01-14 20:28:40 +0700

    fix weight for card

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -4,18 +4,87 @@ import { CardCustom } from './component';
 export const Main =  () => {
   const a = 2
   return (
-    <div style={{ display:'flex',flexWrap:'wrap'}}>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
-      <CardCustom/>
+    <div style={{padding:'20px'}}>
+      <div style={{padding:'20px'}}>
+        <div>
+        <p style={{ fontSize:'24px'}}>Trending Right Now</p>
+        </div>
+        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
+          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+          </div>
+        </div>
+      </div>
+      <div style={{padding:'20px'}}>
+        <div>
+          <p style={{ fontSize:'24px'}}>Based on faculty</p>
+        </div>
+        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
+          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+          </div>
+        </div>
+      </div>
+      <div style={{padding:'20px'}}>
+        <div>
+          <p style={{ fontSize:'24px'}}>Personal Recommendation</p>
+        </div>
+        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
+          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+          </div>
+        </div>
+      </div>
+      <div style={{padding:'20px'}}>
+        <div>
+          <p style={{ fontSize:'24px'}}>Personal Recommendation by Catagory</p>
+        </div>
+        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
+          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+            <CardCustom/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
