651265e4a0a78cb93dcd4503a07405ac8383a241
Boss <boss_12389@hotmail.com>
2023-03-09 22:20:20 +0700

    fix css

--- frontend/src/pages/main/component.jsx
+++ frontend/src/pages/main/component.jsx
@@ -5,13 +5,14 @@ import { faker } from '@faker-js/faker';
 export const CardCustom = ()=>{
   return(
     <Card style={{ width: '18rem',margin:'10px',height:'200px' }}>
-      <Card.Body>
-        <Card.Title>{faker.name.fullName()}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Catagory</Card.Subtitle>
-        <Card.Text>
-          {faker.music.genre()}
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
+      <Card.Body style={{ justifyContent:'space-between', display:'flex', flexDirection:'column'}}>
+        <div>
+          <Card.Title>{faker.name.fullName()}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Catagory</Card.Subtitle>
+          <Card.Text>
+            {faker.music.genre()}
+          </Card.Text>
+        </div>
         <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
--- frontend/src/pages/main/index.jsx
+++ frontend/src/pages/main/index.jsx
@@ -1,90 +1,38 @@
 import react from 'react'
 import { CardCustom } from './component';
 
+const ListBook = ({title}) =>{
+  return (
+  <div style={{padding:'20px'}}>
+  <div style={{width:'fit-content'}}>
+  <p style={{ fontSize:'24px',padding:'10px', borderRadius:'10px', backgroundColor:'#dd5c8e', color:'white'}}>{title}</p>
+  </div>
+  <div style={{ overflow:'auto',width:'fit-content' }}>
+    <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+      <CardCustom/>
+    </div>
+  </div>
+</div>
+)
+}
 export const Main =  () => {
   const a = 2
   return (
-    <div style={{padding:'20px'}}>
-      <div style={{padding:'20px'}}>
-        <div>
-        <p style={{ fontSize:'24px'}}>Trending Right Now</p>
-        </div>
-        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
-          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-          </div>
-        </div>
-      </div>
-      <div style={{padding:'20px'}}>
-        <div>
-          <p style={{ fontSize:'24px'}}>Based on faculty</p>
-        </div>
-        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
-          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-          </div>
-        </div>
-      </div>
-      <div style={{padding:'20px'}}>
-        <div>
-          <p style={{ fontSize:'24px'}}>Personal Recommendation</p>
-        </div>
-        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
-          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-          </div>
-        </div>
-      </div>
-      <div style={{padding:'20px'}}>
-        <div>
-          <p style={{ fontSize:'24px'}}>Personal Recommendation by Catagory</p>
-        </div>
-        <div style={{ backgroundColor:'blue',overflow:'auto',width:'68%'}}>
-          <div style={{ display:'flex',flexWrap:'wrap',height:'220px'}}>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-            <CardCustom/>
-          </div>
-        </div>
-      </div>
+    <div style={{padding:'20px', backgroundColor:'#fee9e8'}}>
+      <ListBook title={'Trending Right Now'}/>
+      <ListBook title={'Based on faculty'}/>
+      <ListBook title={'Personal Recommendation'}/>
+      <ListBook title={'Personal Recommendation by Catagory'}/>
     </div>
   );
 }
